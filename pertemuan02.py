605a260dc8c18687931356b24afba957e6e37463
eduardusoktario <oktario.11@students.amikom.ac.id>
2021-01-02 14:48:24 +0700

    berisi tentang basic, tipe data, dan operator

---
+++ pertemuan02.py
@@ -0,0 +1,40 @@
+#Mencetak kata i love you
+
+# '''Jadi di sesi 1 ini kita belajar 
+# tentang basic dalam python, tipe 
+# data, dan beberapa operator.'''
+
+# x = 20
+# angka_satu = 20
+
+# print("I love you")
+
+# kata_pertama = "Hello World"
+# print(kata_pertama)
+
+# pesan = """Halo selamat siang. Apa kabar? 
+# Aku harap baik baik saja. Selamat Tahun Baru
+# Semoga tahun ini kita dapat bertemu di kampus.
+# Aku udah kangen kamu."""
+# print(pesan)
+
+# angka_satu = 2
+# angka_dua = 21
+# penjumlahan = angka_satu + angka_dua
+# print(angka_satu + angka_dua)
+# print(penjumlahan)
+# print(angka_satu - angka_dua)
+
+# print(type(21/3))
+# print(int(21//3.5))
+
+# a = 10
+# b = 8
+# logika = a >= b
+# #logikanya adalah apakah 10 lebih dari atau sama dengan 8
+# logika2 = a <= b
+# #logikanya adalah apakah 10 kurang dari atau sama dengan 8
+# print(logika)
+# print(logika2)
+
+print("Oktario \n" * 10)
