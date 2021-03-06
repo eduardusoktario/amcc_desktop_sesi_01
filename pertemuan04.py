dc8d9b142348c754fe6ae430d7f9f18efec05e55
eduardusoktario <oktario.11@students.amikom.ac.id>
2021-03-06 13:36:49 +0700

    Basic Input pada Python

---
+++ pertemuan04.py
@@ -0,0 +1,10 @@
+nama = input("Silahkan masukan nama Anda = ")
+# nim = input("Silahkan masukan nim Anda = ")
+
+print("Hai, namaku ", nama)
+
+
+# nama = ()
+# input dalam nama, misalnya Rio
+# nama = Rio
+# ketika di print, itu yang muncul bukan () tetapi Rio
