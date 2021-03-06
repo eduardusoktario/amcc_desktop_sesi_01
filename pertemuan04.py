14e46f963fa545d7b6b0ef92a51ee730c8005fd6
eduardusoktario <oktario.11@students.amikom.ac.id>
2021-03-06 15:02:07 +0700

    penambahan tentang input output

--- pertemuan04.py
+++ pertemuan04.py
@@ -1,10 +1,23 @@
+# nama = input("Silahkan masukan nama Anda = ")
+# # nim = input("Silahkan masukan nim Anda = ")
+
+# print("Hai, namaku ", nama)
+
+
+# # nama = ()
+# # input dalam nama, misalnya Rio
+# # nama = Rio
+# # ketika di print, itu yang muncul bukan () tetapi Rio
+
+# angka  = int(input("Masukkan Angka = "))
+# # konversi_angka = int(angka)
+# # print ("data = ", konversi_angka, "type =", type(konversi_angka))
+# print ("data = ", angka, "type =", type(angka))
+
+
 nama = input("Silahkan masukan nama Anda = ")
-# nim = input("Silahkan masukan nim Anda = ")
+alamat = input("Silahkan masukan alamat Anda = ")
 
-print("Hai, namaku ", nama)
+print("Hai, namaku {} dan aku berasal dari {}".format(nama, alamat))
 
 
-# nama = ()
-# input dalam nama, misalnya Rio
-# nama = Rio
-# ketika di print, itu yang muncul bukan () tetapi Rio
