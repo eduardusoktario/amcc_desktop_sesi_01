f47b5c376d363d3c5536ce6b6fa781e818fbb8d9
eduardusoktario <oktario.11@students.amikom.ac.id>
2021-03-20 12:31:19 +0700

    percabangan perulangan

---
+++ pertemuan05.py
@@ -0,0 +1,54 @@
+# hasil_nembak = "Diterima"
+
+# if hasil_nembak == "Ditolak":
+#     print("Yok semangat cari lagi :)")
+
+# umur = 18 #datanya statis / tetap
+# umur = int(input("Berapa umur kamu? : ")) #datanya dinamis / berubah-ubah
+
+# if umur >= 17 :
+#     print("Wah udah besar, kamu boleh nonton film itu.")
+# else :
+#     print("Wah masih kecil, nontonnya film lain ya.")
+
+# A = 81 - 100
+# B = 61 - 80
+# C = 41 - 60
+# D = 21 - 40
+# E = 0 - 20
+
+nilai = int(input("Masukan nilai ujian kamu : "))
+if nilai >= 81 :
+    grade = "A"
+elif nilai >= 61 : 
+    grade = "B"
+elif nilai >= 41 :
+    grade = "C"
+elif nilai >= 21 :
+    grade = "D"
+else :
+    grade = "E"
+
+print ("Nilai kamu : ", grade)
+
+# if nilai > 81 :
+#     print ("Selamat, Nilai kamu A")
+# elif nilai > 61 and nilai < 81 : 
+#     print ("Selamat, Nilai kamu B")
+# elif nilai > 41 and nilai < 61 :
+#     print ("Selamat, Nilai kamu C")
+# elif nilai > 21 and nilai < 41 :
+#     print ("Selamat, Nilai kamu D. Semangat!")
+# else :
+#     print ("Nilai kamu E, ngapain aja bos?")
+
+# if nilai >= 81 :
+#     print ("Selamat, Nilai kamu A")
+# elif nilai >= 61 : 
+#     print ("Selamat, Nilai kamu B")
+# elif nilai >= 41 :
+#     print ("Selamat, Nilai kamu C")
+# elif nilai >= 21 :
+#     print ("Selamat, Nilai kamu D. Semangat!")
+# else :
+#     print ("Nilai kamu E, ngapain aja bos?")
